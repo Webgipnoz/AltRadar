77b00b8c7da558a2e48160c2ca161fe743039f56
Slav Lomachynkyi <slavchek.3777@gmail.com>
2025-11-01 18:14:43 +0200

    work with login and logout server

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
 from .forms import CustomUserCreationForm
 
 def register(request):
@@ -10,3 +14,21 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html' , {'form' : form})    
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)     
+            messages.success(request, f"Hello, {user.username}")
+            return redirect('home')
+        else:
+            messages.error(request, 'psw is error')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html' , {'form' : form})        
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
